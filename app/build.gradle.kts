86a3a89ca4e99b2dcafaf4c166ab80030ec0f8ef
meng <545222183@qq.com>
2026-09-23 08:13:23 +0000

    Release 1.2.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.mlingofeed"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.2.6"
+        versionCode = 10
+        versionName = "1.2.7"
     }
 
     signingConfigs {
